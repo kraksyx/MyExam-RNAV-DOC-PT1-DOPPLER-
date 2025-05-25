b9277083a04566f6448db876df321a8fad3635b3
kraksyx <blahblahusdme@gmail.com>
2025-05-25 18:27:07 +0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,68 @@
+
+const questions = [
+  {
+    question: "Doppler navigation systems use ___ to determine aircraft ground speed and drift?",
+    options: ["DVOR", "Phase comparison of signals from ground stations", "Frequency shift in signals reflected from the ground", "DME range measurement"],
+    correct: 2
+  },
+  {
+    question: "In day to day use the greatest cause of error in Doppler is:",
+    options: ["input error", "sea movement error", "weight error", "pitch error"],
+    correct: 0
+  },
+  {
+    question: "The largest error and source of error on a Doppler derived position is:",
+    options: ["Cross track due to compass error", "Along track due to TAS computation error", "Cross track due to errors in drift measurement", "Cross track due to error in drift bias"],
+    correct: 2
+  },
+  {
+    question: "Updating Doppler is undertaken when:",
+    options: ["the co-ordinates are reset to revise the position base on alternative information", "A new database is loaded", "The equipment is re-initialized with the data and time, and the atomic clock is actuated", "all of the above"],
+    correct: 0
+  },
+  {
+    question: "After a long period in memory, Doppler:",
+    options: ["falls back to INS", "requires updating", "should be switched to manual", "will have wandered more over land than over sea"],
+    correct: 1
+  },
+  {
+    question: "Sea bias error:",
+    options: ["is due to the movement of the sea and can be partially corrected by the land/sea switch", "is due to the movement of the sea and is not affected by the land/sea switch", "is due to a change in Doppler shift over water and is not affected by the land/sea switch", "is due to a change in Doppler shift over water and is partially corrected by the land/sea switch"],
+    correct: 3
+  },
+  {
+    question: "With regard to Doppler navigation system:",
+    options: ["updating is the process of entering co-ordinates after taking a fix", "updating can be achieved using VOR/DME fix", "updating must be carried out as soon as possible after a prolonged period in memory", "all of the above are correct"],
+    correct: 3
+  },
+  {
+    question: "What frequencies are allocated to Doppler Navigation systems?",
+    options: ["8.8 Hz and 13.3 Hz", "8.8 KHz and 13.3 KHz", "8.8 MHz and 13.3 MHz", "8.8 GHz and 13.3 GHz"],
+    correct: 3
+  },
+  {
+    question: "A radio facility transmits on a wavelength of 2.22 cm. The facility could be a:",
+    options: ["radio altimeter", "Doppler", "LORAN-C", "DME"],
+    correct: 1
+  },
+  {
+    question: "The 4-beam moving Janus array aligns with the aircraft centre line by:",
+    options: ["rotating the array towards the beams with the smallest Doppler shift", "rotating the array until matched pairs of beams receive the same difference in Doppler shift", "measuring the drift and rotating the array by the same amount", "all of the above"],
+    correct: 1
+  },
+  {
+    question: "Which of the following statements is correct when considering a Doppler navigation system?",
+    options: ["aerial misalignment will produce an error to drift angle", "sea bias can be reduced by using a gyro stabilized aerial array", "sea movement error produces higher frequency, and thus ground speed as indicated will be too low", "all of the above"],
+    correct: 0
+  },
+  {
+    question: "Doppler may unlock over:",
+    options: ["shallow fast running water", "a calm sea", "a desert surface", "ice"],
+    correct: 1
+  },
+  {
+    question: "Which of the following statements is TRUE in respect of Doppler?",
+    options: ["The lower the vehicle speed, the higher the transmission frequency used", "The higher the vehicle speed, the higher the transmission frequency used", "Aerials are stabilized to reduce pitching errors", "Aerials are stabilized to reduce climb errors"],
+    correct: 2
+  }
+];
